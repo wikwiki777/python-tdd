89ca5e1f11cbc611b6355bbcf2cdf6e4fb5934b1
wicked777 <wicky.akloe@gmail.com>
2019-08-08 18:47:08 +0200

    Added ability and test to override coin denominations

--- vending_machine.py
+++ vending_machine.py
@@ -1,9 +1,10 @@
 from byotest import *
 
-coins = [100, 50, 20, 10, 5, 2, 1]
+usd_coins = [100, 50, 25, 10, 5, 1]
+eur_coins = [100, 50, 20, 10, 5, 2, 1]
 
 
-def get_change(amount):
+def get_change(amount, coins=eur_coins):
     change = []
     for coin in coins:
         while coin <= amount:
@@ -24,5 +25,7 @@ test_are_equal(get_change(100), [100])
 test_are_equal(get_change(3), [2, 1])
 test_are_equal(get_change(7), [5, 2])
 test_are_equal(get_change(9), [5, 2, 2])
+test_are_equal(get_change(35, usd_coins), [25, 10])
+
 
 print("All tests pass!")
