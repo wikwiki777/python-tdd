7c4104802c18a106cbdd3a68bce3942dc566a32c
wicked777 <wicky.akloe@gmail.com>
2019-08-08 18:43:07 +0200

    Refactored code

--- vending_machine.py
+++ vending_machine.py
@@ -1,15 +1,11 @@
 from byotest import *
 
+coins = [100, 50, 20, 10, 5, 2, 1]
+
 
 def get_change(amount):
-    if amount == 0:
-        return []
-
-    if amount in [100, 50, 20, 10, 5, 2, 1]:
-        return [amount]
-
     change = []
-    for coin in [100, 50, 20, 10, 5, 2, 1]:
+    for coin in coins:
         while coin <= amount:
             amount -= coin
             change.append(coin)
@@ -27,9 +23,6 @@ test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
 test_are_equal(get_change(3), [2, 1])
 test_are_equal(get_change(7), [5, 2])
-
 test_are_equal(get_change(9), [5, 2, 2])
-
-
 
 print("All tests pass!")
